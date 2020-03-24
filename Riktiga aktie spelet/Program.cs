0283efdb8d9f6e74fa4b3371e06a776193b2f092
alvin jangvik <ajangvik@gmail.com>
2020-03-24 13:51:32 +0100

    Samma ändringar som innan men jag fick inte med koden första gången jag pushade.

---
+++ Riktiga aktie spelet/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Riktiga_aktie_spelet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //aktier
+            Stocks olja = new Stocks("Olja", 5);
+            Stocks bröd = new Stocks("Bröd", 3);
+            Stocks vapen = new Stocks("Vapen", 10);
+        }
+    }
+}
---
+++ Riktiga aktie spelet/Stocks.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Riktiga_aktie_spelet
+{
+    class Stocks
+    {
+        private string name;
+        private int portfolio; //Hur mycket man äger av aktien
+        private int change; //Hur mycket aktien kan förändras
+        private bool active; //Om man har investerat
+        private int money; //Hur mycket man investerade från början
+        private int specialChange; //För att event ska kunna påvärka hur aktien ändras
+        public Stocks(string n, int c)
+        {
+            name = n;
+            portfolio = 0;
+            change = c;
+            active = false;
+            money = 0;
+        }
+
+        public int Portfolio
+        {
+            get { return portfolio; }
+            set { portfolio = value; }
+        }
+
+        public int Change
+        {
+            get { return change; }
+            set { change = value; }
+        }
+
+        public bool Active
+        {
+            get { return active; }
+            set
+            {
+                if(value==true || value == false)
+                {
+                    active = value;
+                }
+                else
+                {
+                    Console.WriteLine("Nu är en active fel!!", Console.ForegroundColor = ConsoleColor.Red);
+                }
+            }
+        }
+
+        public int Money
+        {
+            get { return money; }
+            set { money = value;  }
+        }
+    }
+}
