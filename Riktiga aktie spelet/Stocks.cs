10cdda67252974e8fc7d604642af8124883ccf33
AlvinJangvik <54435532+AlvinJangvik@users.noreply.github.com>
2020-05-26 13:14:18 +0200

    Lade till xml kommentarer
    Visual studio gav mig error när jag försökte pusha så jag laddar upp dem manuellt nu

--- Riktiga aktie spelet/Stocks.cs
+++ Riktiga aktie spelet/Stocks.cs
@@ -15,7 +15,13 @@ namespace Riktiga_aktie_spelet
         private int money; //Hur mycket man investerade från början
         private int potential; //Sannolikheten att det går bra för en aktie
         private int tempChange; //Visar hur mycket en aktie har gått ner eller upp sen förra rundan
-
+        
+        /// <summary>
+        /// En klass som tar hand om alla variablar för aktierna som gör det lätt och smidigt att skapa nya aktier.
+        /// </summary>
+        /// <param name="n">Namnet på aktien</param>
+        /// <param name="c">Hur mycket aktien kan förändra varje runda</param>
+        /// <param name="s"> Ser till att en aktie har större eller mindre chans att bli positiv</param> 
         public Stocks(string n, int c, int s)
         {
             name = n;
